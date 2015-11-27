9229ffa3a3d63553794ebf0ad532066f3545af09
seba navarro <sebanavarro@gmail.com>
2015-11-27 12:41:10 -0300

    Navigation Added

--- geoLox/AddReportController.swift
+++ geoLox/AddReportController.swift
@@ -2,8 +2,24 @@
 //  AddReportController.swift
 //  geoLox
 //
+//  Add report... take picture, set location... add description. Submit
+//
 //  Created by snavarro on 11/25/15.
 //  Copyright © 2015 snavarro. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class AddReportController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
--- geoLox/ReportsListController.swift
+++ geoLox/ReportsListController.swift
@@ -2,6 +2,9 @@
 //  ViewController.swift
 //  geoLox
 //
+//
+//
+//
 //  Created by snavarro on 11/25/15.
 //  Copyright © 2015 snavarro. All rights reserved.
 //
